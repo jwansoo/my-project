a897d6940b5d12288d84d66779d76039647c833a
wansoo <jwansoo@hanmail.net>
2022-05-29 16:56:07 +0900

    my-project

---
+++ dist/precache-manifest.e609e55ad98d0c579ad4c54a5fbf3def.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603767ab64a4216570bf",
+    "url": "/css/chunk-vendors.f9baab0f.css"
+  },
+  {
+    "revision": "ad375be1353eb0f5378205f3c932d6bc",
+    "url": "/img/hello-pwa.ad375be1.png"
+  },
+  {
+    "revision": "36154343c6a1f02440511ab44f3f62c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12d49560f19f83f6e5c8",
+    "url": "/js/app.ad3f3692.js"
+  },
+  {
+    "revision": "603767ab64a4216570bf",
+    "url": "/js/chunk-vendors.9ffe6bf5.js"
+  },
+  {
+    "revision": "646d6a5e25fcabefdb7e8e08191568b5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
